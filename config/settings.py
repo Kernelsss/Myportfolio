0689bf340982a04f8a563ec33081be0325080acb
Руслан Ядров <ruslankernels@MacBook-Pro-Ruslan.local>
2026-08-07 15:03:28 +0300

    Смена имени приложения на django_recaptcha

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',  # добавляем наше приложение portfolio
-    'captcha',
+    'django_recaptcha',
 ]
 
 MIDDLEWARE = [
--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import ContactMessage
 from django.conf import settings
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Checkbox
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
 class ContactForm(forms.ModelForm):
     if settings.USE_CAPTCHA:
